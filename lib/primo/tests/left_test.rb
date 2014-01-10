073bca73201ae06f872a2dc2e0a5ad5544f0f4c4
carolina116 <cg116@nyu.edu>
2014-01-10 17:31:56 -0500

    Initial commit of left_test.rb (dlovins)

---
+++ lib/primo/tests/left_test.rb
@@ -0,0 +1,30 @@
+=begin rdoc
+== Daniel
+Tests for Primo daniel results page.
+
+=end
+require File.expand_path("#{File.dirname(__FILE__)}/../test_case")
+module Primo::Daniel
+  # Test full details page for common elements
+  def test_daniel
+    @current_test_name = "Primo - Testing Left-Anchored Author Search for Case-Sensitivity?"
+    each_driver do
+      each_view_default_precision_search do |search_term|
+        break unless (@view.eql? "NYU" and @tab.eql? "all")
+        # Set precision
+        set_precision "exact"
+        set_scope1 "creator"
+        # Search Primo
+        submit_search "Lincoln, Abraham"
+        # Need to add step to Click on FRBR-group link
+        # Click first link
+        click_details_link 1
+        # Make sure the common elements are there
+        common_elements?
+        # sleep 5
+        assert_equal("Douglas an enemy to the North reasons why the North should oppose Judge Douglas : speech of Hon. Abraham Lincoln, of Illinois, delivered at Cincinnati, September 19, 1859.", record_title, "Bummer. No match")
+        
+      end
+    end
+  end
+end
